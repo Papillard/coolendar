b12fceb39fd2a7187215ba59b5461ccac2f6304f
Boris Paillard <boris.paillard@gmail.com>
2013-02-08 16:52:49 +0100

    database config prod

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,17 +3,22 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do  
   
   if Rails.env.production?
+    provider :facebook, '436226379788184', '70622a84dfff2cc5848bfce5d9e939f8',
+    {:scope => 'user_about_me,offline_access,email'}
+       
+    provider :google_oauth2, '149937658688.apps.googleusercontent.com', 'Frr4M60r5xjbBSnHEWLt5Gjl',{
+    access_type: 'offline',
+    scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+    redirect_uri:'http://coolendar.herokuapp.com/auth/google_oauth2/callback'}
 
   else
     provider :facebook, '475199592541938', '25b9a11fb2cd0bb467b070c6ab02f3f4',
     {:scope => 'user_about_me,offline_access,email'}
-   
-    provider :google_oauth2, '230636480839.apps.googleusercontent.com', 'vX3KNICpFdTx1I2e_RkBP9ZH', {access_type: 'online', approval_prompt: ''}
-    
-    #provider :google_oauth2, '230636480839.apps.googleusercontent.com', 'vX3KNICpFdTx1I2e_RkBP9ZH',{
-    #access_type: 'offline',
-    #scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
-    #redirect_url:'http://localhost:3000/auth/google_oauth2/callback'}  
+       
+    provider :google_oauth2, '230636480839.apps.googleusercontent.com', 'vX3KNICpFdTx1I2e_RkBP9ZH',{
+    access_type: 'offline',
+    scope: 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email',
+    redirect_uri:'http://localhost:3000/auth/google_oauth2/callback'}  
     
   end
   
